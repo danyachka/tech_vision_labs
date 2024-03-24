3090460ea04f36d746f8b647cdc14463b2cba3d4
DanilaKisikov <kisikov03@mail.ru>
2024-03-24 05:53:26 +0300

    finish first part of lab2

--- src/lab2/mainLab2.py
+++ src/lab2/mainLab2.py
@@ -2,22 +2,40 @@ import src.lab2.task1 as task1
 
 
 def firstTask():
-    # shift
-    task1.movePixels(20, -30)
 
-    # mirror
-    task1.mirrorImage(Oy=True)
-    task1.mirrorImage(Ox=True, Oy=True)
+    def linear():
+        # shift
+        task1.movePixels(20, -30)
 
-    # scale
-    task1.scale(4, 2)
+        # mirror
+        task1.mirrorImage(Oy=True)
+        task1.mirrorImage(Ox=True, Oy=True)
 
-    # rotate
-    task1.rotateImage(45)
-    task1.rotateImageAroundCenter(45)
+        # scale
+        task1.scale(4, 2)
 
-    # Дальше Аффинное, стр 39
-    task1.affineTransform()
+        # rotate
+        task1.rotateImage(45)
+        task1.rotateImageAroundCenter(45)
+
+        # Аффинное
+        task1.affineTransform()
+
+    def nonLinear():
+        # Скос
+        task1.bevelImage()
+
+        # кусочно-линейное
+        task1.piecewiseLinearTransform(0.3, 3)
+
+        # Проекционное
+        task1.projectiveTransform()
+
+        # Синусоидальное
+        task1.sinTransform()
+
+    #linear()
+    nonLinear()
 
 
 def main():
--- src/lab2/task1.py
+++ src/lab2/task1.py
@@ -94,3 +94,56 @@ def affineTransform():
     result = cv.warpAffine(image, T, (cols, rows))
     utils.showImage(result, "Аффинное отображение")
 
+
+def bevelImage(s=0.3):
+    image = utils.loadDefaultImage()
+    rows, cols = image.shape[0:2]
+
+    T = np.float32([[1, s, 0], [0, 1, 0]])
+    result = cv.warpAffine(image, T, (cols, rows))
+
+    utils.showImage(result, "Скос изображения")
+
+
+def piecewiseLinearTransform(sLeft=0.5, sRight=2):
+    image = utils.loadDefaultImage()
+    rows, cols = image.shape[0:2]
+
+    result = image.copy()
+
+    TLeft = np.float32([[sLeft, 0, 0], [0, 1, 0]])
+    result[:, :int(cols / 2), :] = (
+        cv.warpAffine(result[:, :int(cols / 2), :], TLeft, (cols - int(cols / 2), rows)))
+
+    rightPart = int(sLeft * cols / 2)
+    TRight = np.float32([[sRight, 0, 0], [0, 1, 0]])
+    result[:, rightPart:, :] = (
+        cv.warpAffine(result[:, int(cols / 2):, :], TRight, (cols - rightPart, rows)))
+
+    utils.showImage(result, "Кусочно-линейное преобразование")
+
+
+# Значения букв 4 строчки ниже (стоит подписать в отчёте)
+def projectiveTransform():
+    image = utils.loadDefaultImage()
+    rows, cols = image.shape[0:2]
+
+    ABC = [0.9, 0.15, 0.00075]
+    DEF = [0.29, 1.4, 0.0004]
+    GH1 = [0, 0, 1]
+    T = np.float32([ABC, DEF, GH1])
+
+    result = cv.warpPerspective(image, T, (cols, rows))
+
+    utils.showImage(result, "Проекционное преобразование")
+
+
+def sinTransform():
+    image = utils.loadDefaultImage()
+    rows, cols = image.shape[0:2]
+
+    u, v = np.meshgrid(np.arange(cols), np.arange(rows))
+    u = u + 80 * np.sin(0.5 * np.pi * v / 90)
+    result = cv.remap(image, u.astype(np.float32), v.astype(np.float32), cv.INTER_LINEAR)
+
+    utils.showImage(result, "Синусоидальное преобразование")
